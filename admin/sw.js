4a4fc5997500ac5544a76287aefce9ebae1d1821
irodrigosd <120876080+irodrigosd@users.noreply.github.com>
2026-09-02 02:34:52 -0300

    chore: forçar atualização do PWA para relatórios por coorte

--- admin/sw.js
+++ admin/sw.js
@@ -1,4 +1,4 @@
-const CACHE='mpmv-os-v5';
+const CACHE='mpmv-os-v6';
 const SHELL=['/admin/','/admin/leads360/','/admin/brevo/','/admin/relatorios/','/admin/manifest.webmanifest','/admin/icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
